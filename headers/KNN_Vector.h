01482461f0cd61788837f4ed64f219e99e992396
Renan <renan_battlenet@hotmail.com>
2018-10-19 12:32:38 -0300

    Comentários a respeito da manipulação de vetores.

--- headers/KNN_Vector.h
+++ headers/KNN_Vector.h
@@ -1,7 +1,14 @@
+// ALOCA UM VETOR DINAMICAMENTE
 int* create_I_Vector(int length);
+// LIBERA UM VETOR DA MEMORIA
 void free_I_Vector(int* v);
+// SOMA O VETOR V COM O VETOR W
 int* sum_I_Vector(int *v, int *w, int len);
+// SUBTRAI O VETOR W DO VETOR V
 int* subtract_I_Vector(int *v, int *w, int len);
+// PRODUTO INTERNO ENTRE O VETOR V E O VETOR W
 int* product_I_Vector(int *v, int *w, int len);
+// CRIA UM VETOR QUE É O QUADRADO DE V
 int* square_I_Vector(int *v, int len);
+// IMPRIME O VETOR NA TELA
 void print_Vector(int *v, int len);
--- sources/KNN_Vector.c
+++ sources/KNN_Vector.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// ALOCA UM VETOR DINAMICAMENTE
 int* create_I_Vector(int length){
 	int *v = (int*) malloc(sizeof(int) * length);
 	return v;
 }
-
+// LIBERA UM VETOR DA MEMORIA
 void free_I_Vector(int* v){	free(v); }
-
+// SOMA O VETOR V COM O VETOR W
 int* sum_I_Vector(int *v,int *w, int len){
 	int i = 0;
 	int *z = create_I_Vector(len);
@@ -16,7 +17,7 @@ int* sum_I_Vector(int *v,int *w, int len){
 		z[i] = v[i] + w[i];
 	return z;
 }
-
+// SUBTRAI O VETOR W DO VETOR V
 int* subtract_I_Vector(int *v, int *w, int len){
 	int i = 0;
 	int *z = create_I_Vector(len);
@@ -24,7 +25,7 @@ int* subtract_I_Vector(int *v, int *w, int len){
 		z[i] = v[i] - w[i];
 	return z;	
 }
-
+// PRODUTO INTERNO ENTRE O VETOR V E O VETOR W
 int* product_I_Vector(int *v, int *w, int len){
 	int i = 0;
 	int *z = create_I_Vector(len);
@@ -32,7 +33,7 @@ int* product_I_Vector(int *v, int *w, int len){
 		z[i] = v[i] * w[i];
 	return z;
 }
-
+// CRIA UM VETOR QUE E O QUADRADO DE V
 int* square_I_Vector(int *v, int len){
 	int i = 0;
 	int *w = create_I_Vector(len);
@@ -40,7 +41,7 @@ int* square_I_Vector(int *v, int len){
 		w[i] = v[i] * v[i];
 	return w;
 }
-
+// IMPRIME O VETOR NA TELA
 void print_Vector(int *v, int len){
 	int i = 0;
 	for( i = 0 ; i < len ; i++ )
