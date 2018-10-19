e17e4c83fac37dd883b9fe28fe891bd086b6c602
Renan <renan_battlenet@hotmail.com>
2018-10-19 10:30:30 -0300

    Novas funções para manipulação de vetores e cabeçalho de classificadores.

---
+++ headers/KNN_Classifiers.h
@@ -0,0 +1,1 @@
+float Accuracy(int A, int T);
--- headers/KNN_Vector.h
+++ headers/KNN_Vector.h
@@ -1,2 +1,6 @@
-void Create_I_Vector(int length);
+int* Create_I_Vector(int length);
 void Free_I_Vector(int* v);
+int* Sum_I_Vector(int *v, int *w);
+int* Subtract_I_Vector(int *v, int *w);
+int* Product_I_Vector(int *v, int *w);
+void Square_I_Vector(int *v);
