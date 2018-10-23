7972d0000113a9a569c4b117371f4b9c8a7be5f8
Renan <renan_battlenet@hotmail.com>
2018-10-23 13:56:59 -0200

    Implementação de algumas funções de distância e alterações no cabeçalho.

--- headers/KNN_Distance.h
+++ headers/KNN_Distance.h
@@ -1,6 +1,6 @@
 // CALCULA A DISTANCIA EUCLIDIANA
-float EuclidianDistance(int *v, int *w);
+float EuclidianDistance(int *v, int *w, int len);
 // CALCULA A DISTANCIA DE MINKOWSKY
-float MinkowskyDistance(int *v, int *w);
+float MinkowskyDistance(int *v, int *w, int len, int r);
 // CALCULA D SITANCIA PELA SIMILARIDADE DOS COSSENOS
-float cosinesDistance(int *v, int *w);
+float cosinesDistance(int *v, int *w, int len);
---
+++ sources/KNN_Distance.c
@@ -0,0 +1,50 @@
+#include "KNN_Vector.h"
+#include <math.h>
+
+// CALCULA A DISTANCIA EUCLIDIANA
+float EuclidianDistance(int *v, int *w, int len){
+	int i = 0;
+	int *z;
+	int *z_square;
+	float distance = 0;
+
+	z = subtract_I_Vector(v, w, len);
+
+	z_square = square_I_Vector(z, len);
+
+	free_I_Vector(z);
+
+	distance = sqrt(sum_Elements_I_Vector(z_square, len));
+
+	free_I_Vector(z_square);
+
+	return distance;
+}
+// CALCULA A DISTANCIA DE MINKOWSKY
+float MinkowskyDistance(int *v, int *w, int len, int r){
+	int i = 0;
+	int *z;
+	int *z_aux;
+	float distance = 0;
+
+	z = subtract_I_Vector(v, w, len);
+
+	z_aux = abs_I_Vector(z, len);
+
+	free_I_Vector(z);
+
+	z = pow_I_Vector(, len, r);
+
+	free_I_Vector(z_aux);
+
+	distance = pow(sum_Elements_I_Vector(z ,1.0/r);
+
+	free_I_Vector(z);
+
+	return distance;
+}
+// CALCULA A DISTANCIA PELA SIMILARIDADE DOS COSSENOS
+float cosinesDistance(int *v, int *w, int len){
+
+}
+
